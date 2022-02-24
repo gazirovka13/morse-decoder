e506cc864f31c480ee8596dab9791c187be4b2f6
Anna Ivanyk <annivanyk@gmail.com>
2022-02-25 00:42:25 +0300

    feat: add function decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let mas = [];
+    let str = "";
+    let ch = "";
+    for(let i=0;i<expr.length;i+=10){
+        mas.push(expr.slice(i, i+10));
+    }
+    for(let elem of mas){
+        if (elem === "**********")
+            str += " ";
+        else{
+            let i = 0;
+            ch="";
+            while(elem[i]==='0')
+                i++;
+            for(;i<elem.length;i+=2){
+                if(elem.slice(i, i+2) === "10")
+                    ch+=".";
+                else
+                    ch+="-";
+            }
+            str+=MORSE_TABLE[ch];
+        }
+    }
+    return str;
+
 }
 
 module.exports = {
